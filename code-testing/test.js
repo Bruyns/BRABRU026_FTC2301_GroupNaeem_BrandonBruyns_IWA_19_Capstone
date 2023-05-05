3b753868c2e392b1ffcb9b20fe2cbc2d7cc34994
Bruyns <brandon.bruyns7@gmail.com>
2023-05-05 15:57:48 +0200

    editted syntax errors but have yet to have an output be achieved

--- code-testing/test.js
+++ code-testing/test.js
@@ -7,9 +7,10 @@ const handleSearchcancel = () => {
 }
 
 
+data.search.cancel.addEventListener('click', handleSearchcancel)
 
 data-search-cancel.click(document.querySelector(`[data-search-overlay]`).open === false )
-data.search.cancel.addEventListener('click', handleSearchcancel)
+
 
 
 data-settings-cancel.click() { document.querySelector(`[data-settings-overlay]`).open === false }
@@ -18,3 +19,51 @@ data-list-close.click() { document.querySelector(`[data-list-active]`).open === 
 
 
 
+// fetchs files from the data.js file to be used in the script.js scripts
+// might improve performance of webpage loadtimes as it runs alongside the html and runs the data.js file key data points in the script.js file
+
+if (!books && !Array.isArray(books)) {
+    throw new Error('Source required')};
+if (!range && range.length < 2) {
+    throw new Error('Range must be an array with two numbers')};
+
+// console.log(typeof(matches))
+// console.log checks whether the typeof(matches) is actually a string, array
+
+// light and dark mode rgb ranges to be used by the user depending on preference
+const cssThemeMode = {
+day: {
+    dark: '10, 10, 20',
+    light: '255, 255, 255',
+},
+
+night: {
+    dark: '255, 255, 255',
+    light: '10, 10, 20',
+    },
+}
+
+const previewFragment = document.createDocumentFragment();
+const extractedSearch = books.slice(0, 36);
+const [ authors, id, image, title ] = matches;
+
+
+// for the "for" loop set the end of loop to 36 so it stops showing books when it reachs "extracted = 36"
+// make {author, image, title, id} into an array so that they are easier to access
+for (let i = 0; i < extractedSearch.length ; i++) {
+    const preview = {
+        author: extractedSearch[i].author,
+        id: extractedSearch[i].id,
+        image: extractedSearch[i].image,
+        title: extractedSearch[i].title,
+    }
+
+    const previewElement = document.createElement("div");
+    previewElement.innerText =//  start off by declaring all const to be used throughout the script in the global scope
+// 
+import{
+    BOOKS_PER_PAGE, authors, genres, books, 
+}from "./data"
+
+const matches = books
+const page = 1;
--- scripts.js
+++ scripts.js
@@ -1,119 +1,180 @@
 //  start off by declaring all const to be used throughout the script in the global scope
 // 
+import{
+    BOOKS_PER_PAGE, authors, genres, books, 
+}from "./data"
+
 const matches = books
 const page = 1;
-import{
-    BOOKS_PER_PAGE, author, genres, books, 
-}from "./data"
+
 // fetchs files from the data.js file to be used in the script.js scripts
 // might improve performance of webpage loadtimes as it runs alongside the html and runs the data.js file key data points in the script.js file
 
-if (!books && !Array.isArray(books)) throw new Error('Source required') 
-if (!range && range.length < 2) throw new Error('Range must be an array with two numbers')
+if (!books && !Array.isArray(books)) {throw new Error('Source required')} 
+if (!books && books.length < 2) {throw new Error('Range must be an array with two numbers')}
 
-console.log(typeof(matches))
+// console.log(typeof(matches))
 // console.log checks whether the typeof(matches) is actually a string, array
 
 // light and dark mode rgb ranges to be used by the user depending on preference
-day = {
+const cssThemeMode = {
+day: {
     dark: '10, 10, 20',
     light: '255, 255, 255',
-}
+},
 
-night = {
+night: {
     dark: '255, 255, 255',
     light: '10, 10, 20',
+    },
 }
 
 const previewFragment = document.createDocumentFragment();
 const extractedSearch = books.slice(0, 36);
-const [author, id, image, title] = matches
+const [ authors, id, image, title ] = matches;
 
 
 // for the "for" loop set the end of loop to 36 so it stops showing books when it reachs "extracted = 36"
 // make {author, image, title, id} into an array so that they are easier to access
-for (let i = 0; i <= extractedBooks ; i++) {
+for (let i = 0; i < extractedSearch.length ; i++) {
     const preview = {
-        author,
-        id,
-        image,
-        title,
+        author: extractedSearch[i].author,
+        id: extractedSearch[i].id,
+        image: extractedSearch[i].image,
+        title: extractedSearch[i].title,
     }
 
-    previewFragment.appendChild(preview);
-    return previewFragment;
+    const previewElement = document.createElement("div");
+    previewElement.innerText = `
+    <img src="${preview.image}" alt="${preview.title}">
+    <h2>${preview.title}</h2>
+    <p>${preview.author}</p>
+  `;
 }
 
-data-list-items.appendChild(fragment)
+const listItems = document.querySelector("list__items");
+    listItems.appendChild(previewFragment);
+
+// const dataListItems = document.querySelector(`[data-list-items]`).appendChild(fragment)
+
+
+
+// GENRES LOOP FUNCTION
 // creates a option button in the html with a nested object giving value an assignment of "any" or "all genres"
-const genres = document.createDocumentFragment()
-const element = document.createElement('option')
-  element.value = 'any'
-  element = 'All Genres'
-  genres.appendChild(element)
+const genresFrag = document.createDocumentFragment()
+const genresElement = document.createElement('option')
+genresElement.value = 'any'
+genresElement.innerText = 'All Genres'
+genresFrag.appendChild(genresElement)
 // for loop to dislay a list of values(books/any-genres) to output to the element
 // how much should be displayed in the html/webpage.
 // sets the amount of times the for loop should run according to the amount of entries that should be presented
-for (let [id, name] = 0; [] <= Object.entries(genres); i++) {
-    document.createElement('option')
-    element.value = value
-    element.innerText = text
-    genres.appendChild(element)
+for (const [id, name] of Object.entries(genres)) {
+    const genresElement = document.createElement('option')
+    genresElement.value = id;
+    genresElement.innerText = name;
+    genresFrag.appendChild(genresElement)
 }
 // sends the data-key searchs to the DOM? for genres in the data.js
-data-search-genres.appendChild(genres)
+console.log(genreElement)
 
-const authors = document.createDocumentFragment()
-element = document.createElement('option')
-element.value = 'any'
-element.innerText = 'All Authors'
-authors.appendChild(element)
+const dataSearchGenre = document.querySelector('[data-search-genres]').appendChild(genresFrag);
+console.log(dataSearchGenre)
+
+// AUTHORS LOOP FUNCTION
+// creates a recurring loo funciton for the author key
+const authorsFrag = document.createDocumentFragment()
+const authorElement = document.createElement('option')
+authorElement.value = 'any'
+authorElement.innerText = 'All Authors'
+authorsFrag.appendChild(authorElement)
 // sets the loop limit for how many author entries should be displayed on the webpage
 // send the given amount to the element/ DOM?
-for (let [id, name] = 0 ; [] <= Object.entries(authors); id++) {
-    document.createElement('option')
-    element.value = value
-    element = text
-    authors.appendChild(element)
+for (const [id, name] of Object.entries(authors)) {
+    const authorElement = document.createElement('option');
+    authorElement.value = id;
+    authorElement.innerText = name;
+    authorsFrag.appendChild(authorElement);
 }
 
-data-search-authors.appendChild(authors)
+const dataSearchAuthor = document.querySelector('[data-search-authors]').appendChild(authors);
 
+
+// CSS SELECTORS
+// CSS that checks whether the theme should be night or day depending on whether the data key returns true or false
 data-settings-theme.value === window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ? 'night' : 'day'
-v = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches? 'night' || 'day'
+const v = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches? 'night' : 'day'
 
+
+const dataSettingsTheme = document.querySelector(`[data-settings-theme]`)
 // do you make this into a object to set light and dark theme?
-documentElement.style.setProperty('--color-dark', css[v].dark);
-documentElement.style.setProperty('--color-light', css[v].light);
-const dataListButton = document.querrySelector(`[data-list-button]`)
-console.log(matches.length)
+dataSettingsTheme.style.setProperty('--color-dark', css[v].dark);
+dataSettingsTheme.style.setProperty('--color-light', css[v].light);
+// console.log(matches.length)
 //  = "Show more (books.length - BOOKS_PER_PAGE)"
 
-if (data-list-button.disabled) { !(matches.length - [page * BOOKS_PER_PAGE] > 0)
-    // else {(data-list-button.enabled)
-    // }
-
-data-list-button.innerHTML = /* html */ [
-    '<span>Show more</span>',
-    '<span class="list__remaining"> (${matches.length - [page * BOOKS_PER_PAGE] > 0 ? matches.length - [page * BOOKS_PER_PAGE] : 0})</span>',
-]
-}
-
+// 
+// a boolean statement that checks whether the data list button is enabled or disabled.
+/* when the value of remaining books are less then 0 the .disabled is true which disables the 
+button but while the remaining books are greater then 0 it will output the remaining number 
+according to the equation as a span in the innerHTML*/
+const dataListButton = document.querySelector(`[data-list-button]`)
+if (matches.length - (page * BOOKS_PER_PAGE) <= 0) {
+    dataListButton.disabled = true;
+    dataListButton.innerHTML = /* HTML */
+    `<span>...<span>`
+} else {
+    dataListButton.innerHTML = /* html */ 
+    ` 
+  <span>Show more</span>
+   <span class="list__remaining"> ${matches.length - [page * BOOKS_PER_PAGE] > 0 ? matches.length - [page * BOOKS_PER_PAGE] : ''}</span>
+   `
+ }
 // listeners,
 //  should i add addeventlisteners here so that the js fires to the html whenever a button is pressed
 // for the data-list?
 // do we add an event listener to the list button within the css or the html
-data-search-cancel.click(document.querySelector(`[data-search-overlay]`).open === false )
-data-settings-cancel.click(document.querySelector(`[data-settings-overlay]`).open === false)
-data-settings-form.submit(document.querySelector(`[actions.settings.submit]`))
-data-list-close.click(document.querySelector(`[data-list-active]`).open === false)
+const searchOverlay = document.querySelector(`[data-search-overlay]`)
+searchOverlay.addEventListener('click',() => {
+ showSearchOverlay();
+})
 
+const SettingsOverlay =  document.querySelector(`[data-settings-overlay]`).open === false 
+const SearchCancel =  document.querySelector(`[data-search-cancel]`)
+const SettingsCancel =   document.querySelector(`[data-settings-cancel]`).open === false
+const SettingsForm =   document.querySelector(`[data-settings-form]`)
+const ListClose =   document.querySelector(`[data-list-close]`).open === false
+
+
+
+
+
+
+
+
+
+
+
+// const dataSearchCancel = document.querySelector(`[data-search-cancel]`).addEventListener
+// const dataSettingsCancel = document.querySelector(`[data-settings-cancel]`).addEventListener
+// dataSettingsCancel.addEventListener(document.querySelector(`[data-settings-overlay]`)
+// const dataSettingsForm = document.querySelector(`[data-settings-form]`).addEventListener
+// dataSettingsForm.addEventListener(document.querySelector(`[actions.settings.submit]`))
+// const dataListClose = document.querySelector(`[data-list-close]`).addEventListener
+// dataListClose.addEventListener(document.querySelector(`[data-list-active]`)
 // function used to set book previews to a given value for each page loaded to be ran each time button is clicked
-data-list-button.click() => {
-    document.querySelector(`[data-list-items]`).appendChild(createPreviewsFragment(matches, page x BOOKS_PER_PAGE, {page + 1} x BOOKS_PER_PAGE))
-    actions.list.updateRemaining()
-    page = page + 1
+
+
+const handleSearchCancel= (event) => {
+    // modal shows dialogue hidden overlays/text 
+    event.preventDefault();
+    html.help.overlay.showModal(); 
+    // creates a listener to check if the button is clicked by the user
+    html.help.cancel.addEventListener("click", () =>{
+        html.help.overlay.close();
+    })
 }
+
 
 data-header-search.click() {
     data-search-overlay.open === true ;
